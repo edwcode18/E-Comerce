a7ad3c7cbf107ec11ed050b1ed1553d7c9f23f93
Edwar Sanchez <edwar.s98@hotmail.com>
2023-10-10 21:27:43 -0500

    Se agrega clase con estructura para auditar cada vez que se hace la creación o modificación en una entidad

---
+++ src/main/java/com/ownsprojects/ecomerce/persistence/audit/AuditableEntity.java
@@ -0,0 +1,29 @@
+package com.ownsprojects.ecomerce.persistence.audit;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class AuditableEntity {
+    @Column(name = "created_date")
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @Column(name = "created_by")
+    @CreatedBy
+    private String createdBy;
+
+    @Column(name = "modified_by")
+    @LastModifiedBy
+    private String modifiedBy;
+}
