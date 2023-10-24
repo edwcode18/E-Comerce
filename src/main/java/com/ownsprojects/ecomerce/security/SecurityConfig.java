8a039c5e8f422932fcd841af9b1a502ef20c3162
Edwar Sanchez <edwar.s98@hotmail.com>
2023-10-24 14:30:07 -0500

    Se implementa la configuración para administrar la seguridad del aplicativo

--- src/main/java/com/ownsprojects/ecomerce/security/SecurityConfig.java
+++ src/main/java/com/ownsprojects/ecomerce/security/SecurityConfig.java
@@ -1,10 +1,64 @@
 package com.ownsprojects.ecomerce.security;
 
+import com.ownsprojects.ecomerce.security.filters.JwtAuthenticationFilter;
+import com.ownsprojects.ecomerce.security.filters.JwtAuthorizationFilter;
+import com.ownsprojects.ecomerce.security.jwt.JwtUtils;
+import com.ownsprojects.ecomerce.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+    @Autowired
+    JwtUtils jwtUtils;
 
+    @Autowired
+    CustomerService customerService;
+
+    @Autowired
+    JwtAuthorizationFilter authorizationFilter;
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
+
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtUtils);
+        jwtAuthenticationFilter.setAuthenticationManager(authenticationManager);
+        jwtAuthenticationFilter.setFilterProcessesUrl("/login");
+
+        return httpSecurity
+                .csrf(config -> config.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement(session -> {
+                    session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                })
+                .addFilter(jwtAuthenticationFilter)
+                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(customerService)
+                .passwordEncoder(passwordEncoder)
+                .and().build();
+    }
 }
