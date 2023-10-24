a9e8c47ceb031e0184fd7980c5617407d67114e9
Edwar Sanchez <edwar.s98@hotmail.com>
2023-10-24 12:15:26 -0500

    Se agrega un metodo en el repositorio para encontrar un customer por el nombre

--- src/main/java/com/ownsprojects/ecomerce/persistence/repository/CustomerRepository.java
+++ src/main/java/com/ownsprojects/ecomerce/persistence/repository/CustomerRepository.java
@@ -4,10 +4,18 @@ import com.ownsprojects.ecomerce.persistence.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 /**
  * Repository for managing customers.
  */
 @Repository
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
+    /**
+     * Get customer by name.
+     * @param name The name of the customer.
+     * @return The customer found.
+     */
+    Optional<CustomerEntity> findByName(String name);
 }
